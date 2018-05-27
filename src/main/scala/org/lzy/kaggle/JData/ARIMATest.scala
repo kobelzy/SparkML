49ce30c16c7c4b7cf53e0b5deea5717307a061bb
liuziyang <616560426@qq.com>
2018-05-27 22:45:35 +0800

    commit

---
+++ src/main/scala/org/lzy/kaggle/JData/ARIMATest.scala
@@ -0,0 +1,22 @@
+package org.lzy.kaggle.JData
+import com.cloudera.sparkts.models.ARIMA
+import org.apache.spark.mllib.linalg.Vectors
+/**
+  * Created by Administrator on 2018/5/27.
+  */
+object ARIMATest {
+  def main(args: Array[String]): Unit = {
+    // The dataset is sampled from an ARIMA(1, 0, 1) model generated in R.
+    val lines = scala.io.Source.fromFile("D:\\WorkSpace\\ScalaWorkSpace\\SparkML\\src\\main\\resources\\data2\\R_ARIMA_DataSet1.csv").getLines()
+    val ts = Vectors.dense(lines.map(_.toDouble).toArray)
+    val ts2 = Vectors.dense(ts.toArray.take(5).toArray)
+    val naVector=Vectors.dense(Array[Double]())
+    println(ts.toArray.mkString(","))
+    println(ts2.toArray.mkString(","))
+    println(ts2.size)
+    val arimaModel = ARIMA.fitModel(1, 0, 1, ts)
+//    println("coefficients: " + arimaModel.coefficients.mkString(","))
+    val forecast = arimaModel.forecast(naVector, 1)
+    println("forecast of next 20 observations: " + forecast.toArray.mkString(","))
+  }
+}
--- src/main/scala/org/lzy/kaggle/JData/TimeSeriesTest.scala
+++ src/main/scala/org/lzy/kaggle/JData/TimeSeriesTest.scala
@@ -3,8 +3,13 @@ package org.lzy.kaggle.JData
 import java.sql.Timestamp
 import java.time.{ZoneId, ZonedDateTime}
 
+import com.cloudera.sparkts.models.{ARIMA, ARIMAModel, GARCH}
 import com.cloudera.sparkts.{DateTimeIndex, DayFrequency, TimeSeriesRDD, UniformDateTimeIndex}
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+
+import scala.collection.mutable
 
 /**
   * Created by Administrator on 2018/5/22.
@@ -15,9 +20,13 @@ object TimeSeriesTest {
       .master("local[*]")
       .getOrCreate()
 spark.sparkContext.setLogLevel("WARN")
-    val arr=Array((Timestamp.valueOf("2018-01-01 00:00:00"),"k1",0.5),
+    val arr=Array(
+      (Timestamp.valueOf("2018-01-01 00:00:00"),"k1",0.5),
       (Timestamp.valueOf("2018-01-02 00:00:00"),"k2",0.7),
-      (Timestamp.valueOf("2018-01-01 00:00:00"),"k1",0.9)
+      (Timestamp.valueOf("2018-01-03 00:00:00"),"k3",0.9),
+      (Timestamp.valueOf("2018-01-02 00:00:00"),"k3",0.5),
+      (Timestamp.valueOf("2018-01-03 00:00:00"),"k1",0.7),
+      (Timestamp.valueOf("2018-01-01 00:00:00"),"k2",0.9)
     )
 
 
@@ -40,8 +49,52 @@ spark.sparkContext.setLogLevel("WARN")
     val hybridTimdeIndex=DateTimeIndex.hybrid(Array(irregularTimeIndex,timeIndex))
 
 
-    val ts_rdd=TimeSeriesRDD.timeSeriesRDDFromObservations(hybridTimdeIndex,df,"time","key","value")
-    println(ts_rdd.collectAsTimeSeries().index.toZonedDateTimeArray().mkString(","))
-    ts_rdd.foreach(println)
+    val ts_rdd=TimeSeriesRDD.timeSeriesRDDFromObservations(timeIndex,df,"time","key","value")
+    val ts_fill_rdd=ts_rdd.fill("zero")
+    df.show()
+    ts_fill_rdd.foreach(println)
+//    println(ts_rdd.collectAsTimeSeries().index.toZonedDateTimeArray().mkString(","))
+    val foreach=arimaModelTrain(ts_fill_rdd,3)
+    foreach.foreach(println)
+  }
+
+
+  def ARCHModelTrain[K](trainTsrdd: TimeSeriesRDD[K],predictedN:Int):RDD[Vector]={
+    val archRDD=trainTsrdd.map{case (key,denseVector)=>
+      (GARCH.fitModel(denseVector),denseVector)
+    }
+    /***预测出后N个的值*****/
+    //构成N个预测值向量，之后导入到holtWinters的forcast方法中
+    val predictedArrayBuffer=new mutable.ArrayBuffer[Double]()
+    var i=0
+    while(i<predictedN){
+      predictedArrayBuffer+=i
+      i=i+1
+    }
+    println(predictedArrayBuffer.toArray.mkString(","))
+    val predictedVectors=Vectors.dense(predictedArrayBuffer.toArray)
+    val foreast: RDD[Vector] =archRDD.map{case (model,vector)=>
+      model.addTimeDependentEffects(vector,vector)
+    }
+    foreast
+  }
+
+
+  def arimaModelTrain[K](trainTsrdd: TimeSeriesRDD[K], predictedN: Int): RDD[Vector] = {
+    /** *参数设置 ******/
+
+    /** *创建arima模型 ***/
+    //创建和训练arima模型.其RDD格式为(ArimaModel,Vector)
+    val arimaAndVectorRdd: RDD[(ARIMAModel, Vector)] = trainTsrdd.map { case (key, denseVector) =>
+      //      (ARIMA.autoFit(denseVector), denseVector)
+      val naVector=Vectors.dense(Array[Double]())
+
+      (ARIMA.autoFit(denseVector,1,0,1), naVector)
+
+    }
+
+    /** *预测出后N个的值 *****/
+    val forecast = arimaAndVectorRdd.map { case (arimaModel, denseVector) => arimaModel.forecast(denseVector, predictedN)    }
+    forecast
   }
 }
