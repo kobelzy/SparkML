5c33415fd4a1b4390c449ed8b46969acf7265e2f
lzy <kobeliuziyang@gmail.com>
2018-05-28 19:14:03 +0800

    commit

--- src/main/scala/org/lzy/kaggle/JData/OrderAndActionCluster.scala
+++ src/main/scala/org/lzy/kaggle/JData/OrderAndActionCluster.scala
@@ -1,7 +1,8 @@
 package org.lzy.kaggle.JData
 
 import com.ml.kaggle.JData.TimeFuture.basePath
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import java.sql.Timestamp
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 /**
   * Auther: lzy
@@ -68,11 +69,34 @@ def getUnionDF(order_df:DataFrame,action_df:DataFrame,user_id:DataFrame,sku_df:D
                     .toDF("o_user_id","o_sku_id","o_date","o_sku_num")
 //    user_id,sku_id,a_date,a_num,a_type
     action_df.select("user_id","sku_id","a_date","a_num","a_type")
+    //用户行为，user_id,sku_id,a_date,a_num,a_type,,,,join,,,,,user_id,age,sex,user_lv_cd
+
     val action$User$Sku_df=action_df.join(user_id,Seq("user_id"),"left")
-            .join(sku_df,Seq("sku_id"),"left")
+            .map{ case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,age:Option[Int],sex:Option[Int],user_lv_cd:Option[Int])=>
+                Row(user_id,sku_id,a_date,a_num,a_type,user_lv_cd.getOrElse(0))
+            }
+            //商品信息,sku_id,price,cate,para_1,para_2,para_3
+            .join(sku_df.select("sku_id","cate"),Seq("sku_id"),"left")
+            .map{ case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,user_lv_cd:Int,cate:Option[Int])=>
+                Row(user_id,sku_id,a_date,a_num,a_type,user_lv_cd,cate.getOrElse(0))
+            }
             .select("user_id","sku_id","a_date","a_num","a_type","user_lv_cd","cate")
     //需要通过用户以及o_date,a_date进行join，如果时间 相同，会被留下来
-   val all_df= action$User$Sku_df.join(order_df,action$User$Sku_df("user_id")===order_new_df("o_user_id") and action$User$Sku_df("a_date")===order_new_df("o_date"),"outer")
+    //订单表，user_id,sku_id,o_id,o_date,o_area,o_sku_num
+    val all_df= action$User$Sku_df.join(order_df,action$User$Sku_df("user_id")===order_new_df("o_user_id") and action$User$Sku_df("a_date")===order_new_df("o_date"),"outer")
+                    .map{case Row(user_id:Option[Int], sku_id:Option[Int] ,a_date:Option[Timestamp],a_num:Option[Int],a_type:Option[Int],user_lv_cd:Option[Int],cate:Option[Int],
+                    o_user_id:Option[Int],o_sku_id:Option[Int],o_date:Option[Timestamp],o_sku_num:Option[Int])=>
+                        //通过数据选择，将两种数据分隔开来，这里优先保证订单中数据的存在，之后才能保证浏览数据的存在。
+                        //如果订单的数据也存在，浏览数据也存在，那么会用订单，但是浏览类型我觉得需要去掉了。嗯，那么就需要分开进行判断了。
+                        val user_id_all=o_user_id.getOrElse(user_id.get)
+                        val date=o_date.getOrElse(a_date.get)
+                        val sku_id_all=o_sku_id.getOrElse(sku_id.get)
+                        val a_num_all=a_num.getOrElse(0)
+                           val a_type_all=a_type.getOrElse(0)
+                            val user_lv_cd_all=user_lv_cd.getOrElse(0)
+                            val cate_all=cate.getOrElse(0)
+                            val o_sku_num_all=o_sku_num.getOrElse(0)
+                    }
 //           .select("user_id","sku_id","a_date","a_num","a_type","user_lv_cd","cate","o_user_id","o_sku_id","o_date","o_sku_num")
     all_df
 }
