851b74e8827cc84a1715212a1817f964e8bfb2b4
lzy <kobeliuziyang@gmail.com>
2018-05-29 17:59:04 +0800

    commit

--- src/main/scala/org/lzy/kaggle/JData/OrderAndActionCluster.scala
+++ src/main/scala/org/lzy/kaggle/JData/OrderAndActionCluster.scala
@@ -1,8 +1,13 @@
 package org.lzy.kaggle.JData
 
 import com.ml.kaggle.JData.TimeFuture.basePath
+import com.sun.org.apache.xml.internal.utils.StringToStringTableVector
 import java.sql.Timestamp
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.feature.{OneHotEncoder, StandardScaler, StringIndexer, VectorAssembler}
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
 
 /**
   * Auther: lzy
@@ -13,8 +18,8 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 
 object OrderAndActionCluster {
-    val basePath = "E:\\dataset\\JData_UserShop\\"
-    //  val basePath = "hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"
+//    val basePath = "E:\\dataset\\JData_UserShop\\"
+      val basePath = "hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"
     val sku = "jdata_sku_basic_info.csv"
     val user_basic = "jdata_user_basic_info.csv"
     val user_action = "jdata_user_action.csv"
@@ -22,30 +27,49 @@ object OrderAndActionCluster {
     val user_comment = "jdata_user_comment_score.csv"
 
 
-        def main(args: Array[String]): Unit = {
-            val spark = SparkSession.builder().appName("names")
-                    .master("local[*]")
-                    .getOrCreate()
-                    spark.sparkContext.setLogLevel("WARN")
-            val orderAndActionCluster = new OrderAndActionCluster(spark)
-            //商品信息,sku_id,price,cate,para_1,para_2,para_3
-            val sku_df = orderAndActionCluster.getSourceData(basePath + sku)
-            //用户信息,user_id,age,sex,user_lv_cd
-            val user_df = orderAndActionCluster.getSourceData(basePath + user_basic).cache()
-            //用户行为，user_id,sku_id,a_date,a_num,a_type
-                val action_df=orderAndActionCluster.getSourceData(basePath+user_action)
-            //订单表，user_id,sku_id,o_id,o_date,o_area,o_sku_num
-            val order_df = orderAndActionCluster.getSourceData(basePath + user_order)
-                    .cache()
-            //评价表,user_id,comment_create_tm,o_id,score_level
-            val comment_df = orderAndActionCluster.getSourceData(basePath + user_comment)
+    def main(args: Array[String]): Unit = {
+        val spark = SparkSession.builder().appName("names")
+//                .master("local[*]")
+                .getOrCreate()
+        spark.sparkContext.setLogLevel("WARN")
+        val orderAndActionCluster = new OrderAndActionCluster(spark)
 
-            val all_df=orderAndActionCluster.getUnionDF(order_df,action_df,user_df,sku_df)
-//            all_df.printSchema()
-//            all_df.show(false)
+        //商品信息,sku_id,price,cate,para_1,para_2,para_3
+        val sku_df = orderAndActionCluster.getSourceData(basePath + sku)
+        //用户信息,user_id,age,sex,user_lv_cd
+        val user_df = orderAndActionCluster.getSourceData(basePath + user_basic).cache()
+        //用户行为，user_id,sku_id,a_date,a_num,a_type
+        val action_df = orderAndActionCluster.getSourceData(basePath + user_action)
+        //订单表，user_id,sku_id,o_id,o_date,o_area,o_sku_num
+        val order_df = orderAndActionCluster.getSourceData(basePath + user_order).cache()
+        //评价表,user_id,comment_create_tm,o_id,score_level
+        val comment_df = orderAndActionCluster.getSourceData(basePath + user_comment)
+
+        //合并数据
+        val all_df = orderAndActionCluster.getUnionDF(order_df, action_df, user_df, sku_df)
+//        //预处理数据中间结果
+val all_df_path = basePath + "cache/all_df"
+        all_df.write.mode(SaveMode.Overwrite).parquet(all_df_path)
+
+        val all_df_cache = spark.read.parquet(all_df_path)
+        //        val trains = all_df_cache.select("a_type", "a_num", "o_num", "user_lv_cd", "cate")       //其中a_num,o_num是连续变量
+        val features_df = orderAndActionCluster.featureProcess(all_df_cache)
+        features_df.show(false)
+//        val kmeans_df=orderAndActionCluster.kmeansTrian(features_df)
+        for(k<-(10 to 100,10) ){
+            for (maxIter<- (10 to 30 ,10)) {
+                val kmeans_df = orderAndActionCluster.kmeansTrianByParam(features_df, k,maxIter)
+                kmeans_df.show(false)
+                val orderClass2Count_df = kmeans_df.select("types", "prediction").filter("types == 1").groupBy("prediction").count().sort("prediction")
+                orderClass2Count_df.show(false)
+                println("orderClass2Count_df的k数量：" + orderClass2Count_df.count())
+            }
+        }
+
     }
 }
-class OrderAndActionCluster(spark:SparkSession) {
+
+class OrderAndActionCluster(spark: SparkSession) {
 
     import spark.implicits._
 
@@ -64,78 +88,126 @@ class OrderAndActionCluster(spark:SparkSession) {
         data
     }
 
-def getUnionDF(order_df:DataFrame,action_df:DataFrame,user_id:DataFrame,sku_df:DataFrame)={
-   val order_new_df= order_df.select("user_id","sku_id","o_date","o_sku_num")
-                    .toDF("o_user_id","o_sku_id","o_date","o_sku_num")
-//    user_id,sku_id,a_date,a_num,a_type
-    //用户行为，user_id,sku_id,a_date,a_num,a_type,,,,join,,,,,user_id,age,sex,user_lv_cd
+    def getUnionDF(order_df: DataFrame, action_df: DataFrame, user_id: DataFrame, sku_df: DataFrame) = {
+        val order_new_df = order_df.select("user_id", "sku_id", "o_date", "o_sku_num")
+                .toDF("o_user_id", "o_sku_id", "o_date", "o_sku_num")
 
-//    val action$User$Sku_df=action_df.join(user_id,Seq("user_id"),"left")
-//            .map{ case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,age:Option[Int],sex:Option[Int],user_lv_cd:Option[Int])=>
-//                Row(user_id,sku_id,a_date,a_num,a_type,user_lv_cd.getOrElse(0))
-//            }
-//            //商品信息,sku_id,price,cate,para_1,para_2,para_3
-//            .join(sku_df.select("sku_id","cate"),Seq("sku_id"),"left")
-//            .map{ case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,user_lv_cd:Int,cate:Option[Int])=>
-//                Row(user_id,sku_id,a_date,a_num,a_type,user_lv_cd,cate.getOrElse(0))
-//            }
-//            .select("user_id","sku_id","a_date","a_num","a_type","user_lv_cd","cate")
-//    //需要通过用户以及o_date,a_date进行join，如果时间 相同，会被留下来
-//    //订单表，user_id,sku_id,o_id,o_date,o_area,o_sku_num
-//    val all_df= action$User$Sku_df.join(order_df,action$User$Sku_df("user_id")===order_new_df("o_user_id") and action$User$Sku_df("a_date")===order_new_df("o_date"),"outer")
-//                    .map{case Row(user_id:Option[Int], sku_id:Option[Int] ,a_date:Option[Timestamp],a_num:Option[Int],a_type:Option[Int],user_lv_cd:Option[Int],cate:Option[Int],
-//                    o_user_id:Option[Int],o_sku_id:Option[Int],o_date:Option[Timestamp],o_sku_num:Option[Int])=>
-//                        //通过数据选择，将两种数据分隔开来，这里优先保证订单中数据的存在，之后才能保证浏览数据的存在。
-//                        //如果订单的数据也存在，浏览数据也存在，那么会用订单，但是浏览类型我觉得需要去掉了。嗯，那么就需要分开进行判断了。
-////                        //商品类别，需要用不同的。
-//
-//                        o_user_id match {
-//                            case Some(id) =>(id,o_date.get,o_sku_id.get,0,0,user_lv_cd.get)
-//                        }
-//                        val user_id_all=o_user_id.getOrElse(user_id.get)
-//                        val date=o_date.getOrElse(a_date.get)
-//                        val sku_id_all=o_sku_id.getOrElse(sku_id.get)
-//                        val a_num_all=a_num.getOrElse(0)
-//                           val a_type_all=a_type.getOrElse(0)
-//                            val user_lv_cd_all=user_lv_cd.getOrElse(0)
-//                            val cate_all=cate.getOrElse(0)
-//                            val o_sku_num_all=o_sku_num.getOrElse(0)
-//                    }
 
-         val action2order_df:DataFrame=  action_df .join(order_new_df,action_df("user_id")===order_new_df("o_user_id") and action_df("a_date")===order_new_df("o_date"),"outer")
-    //如果没有join到，会使用null进行代替。
-//                           .map{case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,o_user_id:Int,o_sku_id:Int,o_date:Timestamp,o_sku_num:Int)=>
-//               user_id match {
-//                   case null =>
-//               }
-//           }
-    println("action2order_df:"+action2order_df.count())
-    val orderNotNull_df=action2order_df .filter($"o_user_id".isNotNull)
-                   .map{row=>
+        val action2order_df: DataFrame = action_df.join(order_new_df, action_df("user_id") === order_new_df("o_user_id") and action_df("a_date") === order_new_df("o_date"), "outer")
+
+        println("action2order_df:" + action2order_df.count())
+        val orderNotNull_df = action2order_df.filter($"o_user_id".isNotNull)
+                .map { row =>
 //                       case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,o_user_id:Int,o_sku_id:Int,o_date:Timestamp,o_sku_num:Int)=>
-                       val userID=row.getInt(5)
-                           val date=row.getTimestamp(7)
-                           val sku=row.getInt(6)
-                           val aType=0
-                           val aNum=0
-                           val oNum=row.getInt(8)
-                       (userID,date,sku,aType,aNum,oNum)
-           }
-    val actionNotNull_df=action2order_df .filter($"user_id".isNotNull).filter($"o_user_id".isNull)
-            .map{row=>
+                    val userID = row.getInt(5)
+                    val date = row.getTimestamp(7)
+                    val sku = row.getInt(6)
+                    val aType = 0
+                    val aNum = 0
+                    val oNum = row.getInt(8)
+                    val types=1
+                    (userID, date, sku, aType, aNum, oNum,types)
+                }
+        val actionNotNull_df = action2order_df.filter($"user_id".isNotNull).filter($"o_user_id".isNull)
+                .map { row =>
 //                case Row(user_id:Int, sku_id:Int ,a_date:Timestamp,a_num:Int,a_type:Int,o_user_id:Int,o_sku_id:Int,o_date:Timestamp,o_sku_num:Int)=>
-                val userID=row.getInt(0)
-                val date=row.getTimestamp(2)
-                val sku=row.getInt(1)
-                val aType=row.getInt(4)
-                val aNum=row.getInt(3)
-                val oNum=0
-                (userID,date,sku,aType,aNum,oNum)
-            }
-    val order2action_df=orderNotNull_df.union(actionNotNull_df).toDF("user_id","date","sku_id","a_type","a_num","o_num")
-    println(order2action_df.count())
-    order2action_df.show()
-}
+                    val userID = row.getInt(0)
+                    val date = row.getTimestamp(2)
+                    val sku = row.getInt(1)
+                    val aType = row.getInt(4)
+                    val aNum = row.getInt(3)
+                    val oNum = 0
+                    val types= 0
+                    (userID, date, sku, aType, aNum, oNum,types)
+                }
 
+        val order2action_df = orderNotNull_df.union(actionNotNull_df).toDF("user_id", "date", "sku_id", "a_type", "a_num", "o_num","types")
+//        println(order2action_df.count())
+//        order2action_df.show()
+
+        val all_df = order2action_df.join(user_id, "user_id")
+                .join(sku_df, "sku_id")
+                .select("user_id", "date", "sku_id", "a_type", "a_num", "o_num", "user_lv_cd", "cate","types")
+//        println("all_df:" + all_df.count())
+//        all_df.show()
+        all_df
+    }
+
+    def featureProcess(df: DataFrame) = {
+        val columns = Array("user_id", "date", "sku_id", "a_type", "a_num", "o_num", "user_lv_cd", "cate")
+        //        val train_df = df.select("a_type", "a_num", "o_num", "user_lv_cd", "cate") //其中a_num,o_num是连续变量
+        var stages = Array[PipelineStage]()
+        val pipeline = new Pipeline()
+        //归一化化
+        val linearColumnsAssambel = new VectorAssembler().setInputCols(Array("o_num", "a_num")).setOutputCol("continuous_variable")
+        val o_num_scaler = new StandardScaler()
+                .setInputCol("continuous_variable").setOutputCol("continuous_variable" + "_scale")
+        stages = stages :+ linearColumnsAssambel :+ o_num_scaler
+        //独热编码
+        val onehotColumns = Array("a_type", "user_lv_cd", "cate")
+        for (column <- onehotColumns) {
+            stages = stages ++ stageByOneHot(column)
+        }
+        //合并向量
+        val vectorAssembler = new VectorAssembler()
+                .setInputCols(onehotColumns.map(_ + "_onehot") :+ "continuous_variable_scale")
+                .setOutputCol("features")
+        stages = stages :+ vectorAssembler
+
+
+        val model = pipeline.setStages(stages)
+                .fit(df)
+        val feature_df = model.transform(df).select("user_id", "date", "sku_id", "cate","types", "features")
+        feature_df
+    }
+
+    def stageByOneHot(column: String): Array[PipelineStage] = {
+        var stages = Array[PipelineStage]()
+        val string2vector = new StringIndexer()
+                .setInputCol(column)
+                .setOutputCol(column + "_string2vector")
+        stages = stages :+ string2vector
+        val onehoter: OneHotEncoder = new OneHotEncoder()
+                .setInputCol(column + "_string2vector").setOutputCol(column + "_onehot")
+        stages = stages :+ onehoter
+        stages
+    }
+
+    def kmeansTrian(df: DataFrame) = {
+        var stages = Array[PipelineStage]()
+        //定义模型
+        val kmeans = new KMeans()
+                .setFeaturesCol("features")
+//        'random' and 'k-means||'
+                .setInitMode("k-means||")
+        stages = stages :+ kmeans
+        val params:ParamMap = ParamMap(kmeans.k -> 30,
+//            kmeans.tol->0.2,  //迭代算法收敛性
+            kmeans.seed -> 1L,
+            kmeans.maxIter -> 10
+        )
+        val pipeline = new Pipeline()
+                .setStages(stages)
+        val model=pipeline.fit(df,params)
+        model.transform(df)
+    }
+
+    def kmeansTrianByParam(df: DataFrame,k:Int,maxIter:Int) = {
+        println("########################################################################")
+        println("k:"+k+"maxIter:"+maxIter)
+        var stages = Array[PipelineStage]()
+        //定义模型
+        val kmeans = new KMeans()
+                .setFeaturesCol("features")
+//        'random' and 'k-means||'
+                .setInitMode("k-means||")
+                        .setK(k)
+                        .setMaxIter(maxIter)
+        stages = stages :+ kmeans
+        val pipeline = new Pipeline()
+                .setStages(stages)
+        val model=pipeline.fit(df)
+        model.transform(df)
+    }
 }
 
