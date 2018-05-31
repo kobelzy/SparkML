660b9d877d110c47ca755a998cd2daa95a2a4c5b
liuziyang <616560426@qq.com>
2018-05-31 22:51:00 +0800

    commit

--- src/main/scala/org/lzy/kaggle/JDataByLeaner/FeaExact.scala
+++ src/main/scala/org/lzy/kaggle/JDataByLeaner/FeaExact.scala
@@ -2,11 +2,8 @@ package org.lzy.kaggle.JDataByLeaner
 
 import java.sql.Timestamp
 
-import kingpoint.timeSeries.time
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.dmg.pmml.False
-import spire.syntax.{action, order}
 import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
   * Created by Administrator on 2018/5/30.
@@ -15,47 +12,65 @@ import org.apache.spark.sql.functions.col
 
 object FeaExact {
   val basePath = "E:\\dataset\\JData_UserShop\\"
+
   //  val basePath = "hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("names")
       .master("local[*]")
       .getOrCreate()
-    val util=new Util(spark)
-    val (order,action)=util.loadData(basePath)
+    val util = new Util(spark)
+    val (order, action) = util.loadData(basePath)
   }
-
 
 
 }
 
-class FeaExact(spark:SparkSession){
+class FeaExact(spark: SparkSession) {
+
   import spark.implicits._
-  def createFeat(startTime:Timestamp,endTime:Timestamp,user_df:DataFrame,
-                 order:DataFrame,action:DataFrame,test:Boolean=false): Unit ={
-    val order_df=order.sort($"o_date")
-    val action_df=action.sort($"a_date")
+
+  def createFeat(startTime: Timestamp, endTime: Timestamp, user_df: DataFrame,
+                 order: DataFrame, action: DataFrame, test: Boolean = false): Unit = {
+    val order_df = order.sort($"o_date")
+    val action_df = action.sort($"a_date")
     //预测目标月份的数据
-    val label_month:Int=endTime.toLocalDateTime.getMonthValue
+    val label_month: Int = endTime.toLocalDateTime.getMonthValue
 
     //计算order和action与预测月份之间的时间差值。
-    order_df.withColumn("day_gap",order_df.select($"o_date".as[Timestamp]).map(date=>(endTime.getTime-date.getTime)/(60*60*24*1000)).col("o_date"))
-    action_df.withColumn("day_gap",action_df.select($"a_date".as[Timestamp]).map(date=>(endTime.getTime-date.getTime)/(60*60*24*1000)).col("a_date"))
-    action_df.select($"a_date".as[Timestamp]).map(date=>(endTime.getTime-date.getTime)/(60*60*24*1000))
+    order_df.withColumn("day_gap", order_df.select($"o_date".as[Timestamp]).map(date => (endTime.getTime - date.getTime) / (60 * 60 * 24 * 1000)).col("o_date"))
+    action_df.withColumn("day_gap", action_df.select($"a_date".as[Timestamp]).map(date => (endTime.getTime - date.getTime) / (60 * 60 * 24 * 1000)).col("a_date"))
+    action_df.select($"a_date".as[Timestamp]).map(date => (endTime.getTime - date.getTime) / (60 * 60 * 24 * 1000))
+    val df_label =
+      if (test) {
+        val df_label = user_df.withColumn("label_1", col("age") * 0 - 1)
+          .withColumn("label_2", col("age") * 0 - 1)
+        df_label
+      } else {
+        //找到用户在目标月份最早的订单日期
+        val order_label = order_df.filter(order_df("o_date") === label_month)
+          .filter(order_df("cate") === 30 || order_df("cate") === 101)
+        val label = order_label.sort("o_date").dropDuplicates("user_id")
+        val df_label_1 = user_df.join(label.select("user_id", "o_date"), Seq("user_id"), "left")
 
- if(test){
-   val df_label=user_df.withColumn("label_1",col("age")*0-1)
-     .withColumn("label_2",col("age")*0-1)
- }else{
-   //找到用户在目标月份最早的订单日期
-   val order_label_1=order_df.filter(order_df("o_date") === label_month)
-     .filter(order_df("cate") === 30 || order_df("cate")===101)
-   val label=order_label_1.sort("o_date").dropDuplicates("user_id")
-   val order_label=user_df.join(label.select("user_id","o_date"),Seq("user_id"),"left")
+        //label_1代表用户在目标月份购买的订单个数，label_2代表用户在一个月的几号下的订单
+        val df_label_2 = Util.featUnique(df_label_1, order_label, Array("user_id"), "o_id", Some("label_1"))
+        val df_label = df_label_2.withColumn("label_2", df_label_2.select($"o_date".as[Timestamp], $"o_day".as[Int]).map(date2day => {
+          if (date2day._1.after(startTime)) {
+            date2day._2
+          } else {
+            0
+          }
+        }).toDF("label_2").col("label_2")).drop("o_date")
+        df_label
+      }
 
-   //label_1代表用户在目标月份购买的订单个数，label_2代表用户在一个月的几号下的订单
+    //总体特征
+    val order_tmp=order.filter($"day_gap" > 0)
+    val action_tmp=action.filter($"day_gap" > 0)
 
- }
 
 
- }
+  }
+
+
 }
--- src/main/scala/org/lzy/kaggle/JDataByLeaner/Util.scala
+++ src/main/scala/org/lzy/kaggle/JDataByLeaner/Util.scala
@@ -1,35 +1,44 @@
 package org.lzy.kaggle.JDataByLeaner
 
 import java.sql.Timestamp
-import breeze.linalg.*
-import com.ml.kaggle.JData.TimeFuture.basePath
-import kingpoint.timeSeries.time
-import org.apache.commons.lang3.StringUtils
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.{DataFrame, SparkSession}
+
 import org.apache.spark.sql.functions.udf
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
   * Created by Administrator on 2018/5/30.
   */
 
-object Util{
+object Util {
+  def main(args: Array[String]): Unit = {
+    val basePath = "E:\\dataset\\JData_UserShop\\"
 
-  def featNunique(df:DataFrame,df_feature:DataFrame,fe:Seq[String],value:String,name:String="")={
-    val df_count:DataFrame=df_feature.groupBy(fe.head,fe.tail:_*).count().select("count",fe:_*).withColumn(value,col("count")*0+1)
-    val df_count_newName=if(StringUtils.isBlank(name)){
-//      fe:+(value + fe.mkString("_","_","_")+"nunique")
-      df_count.withColumnRenamed("count",value + fe.mkString("_","_","_")+"nunique")
-    }else{
-//      fe:+name
-      df_count.withColumnRenamed("count",name)
-    }
-    df.join(df_count_newName,fe,"left").na.fill(0)
+    val spark = SparkSession.builder().appName("names")
+      .master("local[*]")
+      .getOrCreate()
+    val util = new Util(spark)
+    val order = util.getSourceData(basePath + "jdata_user_order_test.csv")
+    val user = util.getSourceData(basePath + "jdata_user_basic_info_test.csv")
+    //测试方法loadData
+    //    val (order, action) = util.loadData(basePath)
+    //    order.show()
+    //    action.show()
+
+    //测试方法featUnique
+    featUnique(user, order, Array("user_id"), "o_id").show(false)
+  }
+
+
+  def featUnique(df: DataFrame, df_feature: DataFrame, fe: Seq[String], value: String, name: Option[String] = None) = {
+    val newName = name.getOrElse(value + fe.mkString("_", "_", "_") + "nunique")
+    val df_count: DataFrame = df_feature.groupBy(fe.head, fe.tail: _*).count().select("count", fe: _*)
+      //.withColumn(value,col("count")*0+1)
+      .withColumnRenamed("count", newName)
+    df.join(df_count, fe, "left").na.fill(0)
   }
 }
-class Util(spark:SparkSession) {
-  import spark.implicits._
+
+class Util(spark: SparkSession) {
 
   /**
     * 获取csv转换为DF
@@ -46,7 +55,7 @@ class Util(spark:SparkSession) {
     data
   }
 
-  def loadData(bathPath:String="hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"):(DataFrame, DataFrame) ={
+  def loadData(basePath: String = "hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"): (DataFrame, DataFrame) = {
     val sku = "jdata_sku_basic_info.csv"
     val user_basic = "jdata_user_basic_info.csv"
     val user_action = "jdata_user_action.csv"
@@ -57,40 +66,40 @@ class Util(spark:SparkSession) {
     //用户信息,user_id,age,sex,user_lv_cd
     val user_df = getSourceData(basePath + user_basic)
     //用户行为，user_id,sku_id,a_date,a_num,a_type
-        val action_df=getSourceData(basePath+user_action)
+    val action_df = getSourceData(basePath + user_action)
     //订单表，user_id,sku_id,o_id,o_date,o_area,o_sku_num
     val order_df = getSourceData(basePath + user_order)
       .cache()
     //评价表,user_id,comment_create_tm,o_id,score_level
     val comment_df = getSourceData(basePath + user_comment)
 
-    val user_action_df=action_df.withColumn("a_year",getYearFromTime(action_df("a_date")))
-      .withColumn("a_month",getMonthFromTime(action_df("a_date")))
-      .withColumn("a_day",getDayFromTime(action_df("a_date")))
+    val user_action_df = action_df.withColumn("a_year", getYearFromTime(action_df("a_date")))
+      .withColumn("a_month", getMonthFromTime(action_df("a_date")))
+      .withColumn("a_day", getDayFromTime(action_df("a_date")))
 
-    val user_order_df=order_df.withColumn("o_year",getYearFromTime(order_df("o_date")))
-      .withColumn("o_month",getMonthFromTime(order_df("o_date")))
-      .withColumn("o_day",getDayFromTime(order_df("o_date")))
+    val user_order_df = order_df.withColumn("o_year", getYearFromTime(order_df("o_date")))
+      .withColumn("o_month", getMonthFromTime(order_df("o_date")))
+      .withColumn("o_day", getDayFromTime(order_df("o_date")))
 
-    val user_comment_df=comment_df.withColumnRenamed("comment_create_tm","c_date")
-      .withColumn("c_year",getYearFromTime(comment_df("c_date")))
-      .withColumn("c_month",getMonthFromTime(comment_df("c_date")))
-      .withColumn("c_day",getDayFromTime(comment_df("c_date")))
+    val user_comment_df = comment_df
+      .withColumn("c_year", getYearFromTime(comment_df("comment_create_tm")))
+      .withColumn("c_month", getMonthFromTime(comment_df("comment_create_tm")))
+      .withColumn("c_day", getDayFromTime(comment_df("comment_create_tm")))
+      .withColumnRenamed("comment_create_tm", "c_date")
 
-
-//    * 把user_order,user_comment,sku,user_info连在一起组成order_comment表
-    val order=    user_order_df.join(sku_df,Seq("sku_id"),"left")
-        .join(user_df,Seq("user_id"),"left")
-      .join(user_comment_df,Seq("user_id","o_id"),"left")
+    //    * 把user_order,user_comment,sku,user_info连在一起组成order_comment表
+    val order = user_order_df.join(sku_df, Seq("sku_id"), "left")
+      .join(user_df, Seq("user_id"), "left")
+      .join(user_comment_df, Seq("user_id", "o_id"), "left")
 
     //把user_action,user_info,sku_info连接一起组成user_action表
-    val action=user_action_df.join(sku_df,Seq("sku_id"),"left")
-      .join(user_df,Seq("user_id"),"left")
-    (order,action)
+    val action = user_action_df.join(sku_df, Seq("sku_id"), "left")
+      .join(user_df, Seq("user_id"), "left")
+    (order, action)
   }
 
-  val getMonthFromTime=udf{(time:Timestamp)=>time.toLocalDateTime.getMonthValue}
-  val getDayFromTime=udf{(time:Timestamp)=>time.toLocalDateTime.getDayOfMonth}
-  val getYearFromTime=udf{(time:Timestamp)=>time.toLocalDateTime.getYear}
+  val getMonthFromTime = udf { (time: Timestamp) => time.toLocalDateTime.getMonthValue }
+  val getDayFromTime = udf { (time: Timestamp) => time.toLocalDateTime.getDayOfMonth }
+  val getYearFromTime = udf { (time: Timestamp) => time.toLocalDateTime.getYear }
 }
 
