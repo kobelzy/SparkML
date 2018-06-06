faaf2c7fa8c846a0b99b10922c546750d176201d
lzy <kobeliuziyang@gmail.com>
2018-06-06 18:38:50 +0800

    commit

--- src/main/scala/org/lzy/kaggle/JDataByLeaner/Model.scala
+++ src/main/scala/org/lzy/kaggle/JDataByLeaner/Model.scala
@@ -1,13 +1,10 @@
 package org.lzy.kaggle.JDataByLeaner
 
-import ml.dmlc.xgboost4j.scala.spark.{XGBoost, XGBoostModel}
-import org.apache.spark.internal.config
-import org.apache.spark.{internal, ml}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.mllib.linalg
-import org.apache.spark.mllib.regression.LabeledPoint
+import ml.dmlc.xgboost4j.scala.spark.{XGBoost, XGBoostEstimator, XGBoostModel}
+import org.apache.spark.ml
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
@@ -17,6 +14,7 @@ object Model {
   val basePath = "hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"
   case class datas(features:ml.linalg.Vector, label:Double)
   def main(args: Array[String]): Unit = {
+    val Array(round,nWorkers)=args
     val spark=SparkSession.builder()
 //      .master("local[*]")
       .appName("model").getOrCreate()
@@ -29,14 +27,14 @@ val data:DataFrame =MLUtils.loadLibSVMFile(spark.sparkContext,basePath+"linear_r
         .map(line=>datas(line.features.asML,line.label)).toDF()
     data.show(false)
     val Array(train,test)=data.randomSplit(Array(0.8,0.2))
-    val result =featModel(train,test)
+    val result =featModel(train,test,round.toInt,nWorkers.toInt)
     result .show(false)
   }
-def featModel(train_df:DataFrame,test_df:DataFrame)={
+def featModel(train_df:DataFrame,test_df:DataFrame,round:Int,nWorkers:Int)={
   //迭代次数
-  val round=50
-  //
-  val nWorkers=10
+//  val round=50
+//  //
+//  val nWorkers=10
   val useExternalMemory=true
 val params=Map(
   "booster"->"gbtree",
@@ -50,6 +48,37 @@ val params=Map(
 )
   val model:XGBoostModel=XGBoost.trainWithDataFrame(train_df,params,round,nWorkers)
   val result:DataFrame=model.transform(test_df)
+
    result
 }
+
+  def fitPredict(train_df:DataFrame,test_df:DataFrame)={
+    //迭代次数
+  val round=50
+//  //
+  val nWorkers=10
+val useExternalMemory=true
+    val xgboostParam=Map(
+      "booster"->"gbtree",
+      "objection"->"reg:linear",
+      "eval_metric"->"rmse",
+      "max_depth"->4,
+      "eta"->0.05,
+      "colsample_bytree"->0.9,
+      "subsample"->0.8,
+      "verbose_eval"->0
+    )
+
+    val xgbEstimator = new XGBoostEstimator(xgboostParam)
+    val paramGrid = new ParamGridBuilder()
+            .addGrid(xgbEstimator.round, Array(20, 50))
+            .addGrid(xgbEstimator.eta, Array(0.1, 0.4))
+            .build()
+    val tv=new TrainValidationSplit()
+            .setEstimator(xgbEstimator)
+            .setEvaluator(new RegressionEvaluator())
+            .setEstimatorParamMaps(paramGrid)
+            .setTrainRatio(0.8)
+    val tvModel=tv.fit(train_df)
+  }
 }
