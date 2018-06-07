d35deb158235de1947f947a7f95b460a45859d0f
lzy <kobeliuziyang@gmail.com>
2018-06-07 18:55:02 +0800

    commit

--- src/main/scala/org/lzy/kaggle/JDataByLeaner/TrainModels.scala
+++ src/main/scala/org/lzy/kaggle/JDataByLeaner/TrainModels.scala
@@ -1,34 +1,47 @@
 package org.lzy.kaggle.JDataByLeaner
 
+import java.io
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import scala.collection.mutable
 /**
-  * Created by Administrator on 2018/6/3.
+  * Created by Administrator on 2018/6/3
+  spark-submit --master yarn-client --queue lzy --driver-memory 2g --conf spark.driver.maxResultSize=2g  \
+--num-executors 10 --executor-cores 4 --executor-memory 7g --jars \
+/root/lzy/xgboost/jvm-packages/xgboost4j-spark/target/xgboost4j-spark-0.8-SNAPSHOT-jar-with-dependencies.jar \
+--class org.lzy.kaggle.JDataByLeaner.TrainModels SparkML.jar
   */
 object TrainModels{
   //  val basePath = "E:\\dataset\\JData_UserShop\\"
   val basePath = "hdfs://10.95.3.172:9000/user/lzy/JData_UserShop/"
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("names")
-            .master("local[*]")
+//            .master("local[*]")
       .getOrCreate()
+    val sc=spark.sparkContext
+    val config=sc.getConf
+//    config.set("spark.driver.maxResultSize","0")
+    config.set("spark.debug.maxToStringFields","100")
+    config.set("spark.shuffle.io.maxRetries","60")
+    config.set("spark.default.parallelism","54")
     spark.sparkContext.setLogLevel("WARN")
     val util = new Util(spark)
     val trainModel=new TrainModels(spark,basePath)
+    trainModel.getTrain()
   }
 }
 class TrainModels(spark: SparkSession, basePath: String) {
 import spark.implicits._
 
 
-  def score(df:DataFrame)={
+  def score(result_df:DataFrame)={
     val udf_getWeight=udf{index:Int=>
       1/(1+math.log(index+1))
     }
 
     val udf_binary=udf{label_1:Int=>if (label_1>0) 1 else 0}
-    val weight_df=df.sort("o_num").withColumn("new_label",udf_binary($"label_1")).drop("label_1").withColumnRenamed("new_label","label_1")
+    val weight_df=result_df.sort("o_num").withColumn("new_label",udf_binary($"label_1")).drop("label_1").withColumnRenamed("new_label","label_1")
       .limit(50000)
           .withColumn("index",monotonically_increasing_id)
       .withColumn("weight",udf_getWeight($"index"))
@@ -42,15 +55,20 @@ import spark.implicits._
 def getTrain()={
 val train=spark.read.parquet(basePath + "cache/vali_train")
   val test=spark.read.parquet(basePath+"cache/vali_test")
-  val dropColumns=Array("user_id","label_1","label_2")
-  val result=test.select("user_id","label_1","label_2")
-  val X=train.drop(dropColumns:_*)
-  val X_pred=test.drop(dropColumns:_*)
-  val y=train.select("label_1")
+  val dropColumns:Array[String]=Array("user_id","label_1","label_2")
+  val featureColumns:Array[String]=train.columns.filterNot(dropColumns.contains(_))
+//  val X=train.drop(dropColumns:_*)
+//  val X_pred=test.drop(dropColumns:_*)
+//  val y=train.select("label_1")
+  val selecter=new VectorAssembler().setInputCols(featureColumns).setOutputCol("features")
+  val train_df=selecter.transform(train)
+  val test_df=selecter.transform(test)
   //为resul通过label_1来计算 添加o_num列，
-
+val s1_df=Model.fitPredict(train_df,test_df,"label_1","o_num")
   //为result通过label_2来计算添加pred_date
+val s2_df=Model.fitPredict(train_df,test_df,"label_2","pred_date")
   //result包括了"user_id","label_1","label_2","o_num","pred_date"
+  val result=s1_df.join(s2_df,"user_id")
   score(result)
    }
   def getResult()={
