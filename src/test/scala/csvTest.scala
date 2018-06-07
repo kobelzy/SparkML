8c4901c5179c3297454eee032ca3a7fe3c1338a9
lzy <kobeliuziyang@gmail.com>
2018-06-07 18:50:13 +0800

    commit

--- src/test/scala/csvTest.scala
+++ src/test/scala/csvTest.scala
@@ -1,7 +1,8 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
 import org.lzy.kaggle.JDataByLeaner.FeaExact.basePath
 import org.lzy.kaggle.JDataByLeaner.Util
-
+import org.apache.spark.sql.functions._
 /**
   * Created by Administrator on 2018/6/2.
   */
@@ -22,8 +23,18 @@ object csvTest {
 //    val order_cache = spark.read.parquet(basePath + "cache/order")
 //order_cache.show(false)
 //    order_cache.dropDuplicates("user_id").show(false)
-val order_df = util.getSourceData(basePath + user_order).sort($"o_date")
-    order_df.show(false)
-    order_df.dropDuplicates("user_id").show()
+val order_df = util.getSourceData(basePath + user_order)
+//        .sort($"o_date")
+//    order_df.show(false)
+//    order_df.dropDuplicates("user_id").show()
+//    val index=1
+//    val fun=udf{data:Int=>data}
+//    val news=order_df.withColumn("index",fun(lit(index)))
+//    news.show(false)
+//    val fun2=udf((age:Int,index:Int)=>age+index)
+//    order_df.withColumn("index",fun2($"o_id",lit(index))).show(false)
+    val s=order_df.withColumn("diff",datediff($"o_date",lit("2017-03-09 00:00:00")))
+    s.show(false)
+
   }
 }
