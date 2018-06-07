9d4a0899c47ff90ff01552c3d16a7c4693a995c5
lzy <kobeliuziyang@gmail.com>
2018-06-07 18:54:47 +0800

    commit

--- src/main/scala/org/lzy/kaggle/JDataByLeaner/Model.scala
+++ src/main/scala/org/lzy/kaggle/JDataByLeaner/Model.scala
@@ -2,6 +2,7 @@ package org.lzy.kaggle.JDataByLeaner
 
 import ml.dmlc.xgboost4j.scala.spark.{XGBoost, XGBoostEstimator, XGBoostModel}
 import org.apache.spark.ml
+import org.apache.spark.ml.{PipelineModel, Transformer}
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.mllib.util.MLUtils
@@ -24,7 +25,7 @@ object Model {
     config.set("spark.default.parallelism","54")
 import spark.implicits._
 val data:DataFrame =MLUtils.loadLibSVMFile(spark.sparkContext,basePath+"linear_regression_data.txt")
-        .map(line=>datas(line.features.asML,line.label)).toDF()
+        .map(line=>datas(line.features.asML,line.label)).toDF("features","labels")
     data.show(false)
     val Array(train,test)=data.randomSplit(Array(0.8,0.2))
     val result =featModel(train,test,round.toInt,nWorkers.toInt)
@@ -36,23 +37,34 @@ def featModel(train_df:DataFrame,test_df:DataFrame,round:Int,nWorkers:Int)={
 //  //
 //  val nWorkers=10
   val useExternalMemory=true
-val params=Map(
-  "booster"->"gbtree",
-  "objection"->"reg:linear",
-  "eval_metric"->"rmse",
-  "max_depth"->4,
-  "eta"->0.05,
-  "colsample_bytree"->0.9,
-  "subsample"->0.8,
-  "verbose_eval"->0
-)
-  val model:XGBoostModel=XGBoost.trainWithDataFrame(train_df,params,round,nWorkers)
-  val result:DataFrame=model.transform(test_df)
+    val xgboostParam=Map(
+        "booster"->"gbtree",
+        "objection"->"reg:linear",
+        "eval_metric"->"rmse",
+        "max_depth"->4,
+//      "eta"->0.05,
+        "colsample_bytree"->0.9,
+        "subsample"->0.8,
+        "verbose_eval"->0
+    )
+    val xgbEstimator = new XGBoostEstimator(xgboostParam).setLabelCol("labels")
+//            .setPredictionCol("result")
 
-   result
+    val paramGrid = new ParamGridBuilder()
+            .addGrid(xgbEstimator.eta, Array(0.05))
+            .build()
+
+    val tv=new TrainValidationSplit()
+            .setEstimator(xgbEstimator)
+            .setEvaluator(new RegressionEvaluator())
+            .setEstimatorParamMaps(paramGrid)
+            .setTrainRatio(0.8)
+    val tvModel=tv.fit(train_df)
+    val result_df=tvModel.transform(test_df).withColumnRenamed("prediction","result")
+    result_df
 }
 
-  def fitPredict(train_df:DataFrame,test_df:DataFrame)={
+  def fitPredict(train_df:DataFrame,test_df:DataFrame,labelCol:String,predictCol:String)={
     //迭代次数
   val round=50
 //  //
@@ -63,22 +75,34 @@ val useExternalMemory=true
       "objection"->"reg:linear",
       "eval_metric"->"rmse",
       "max_depth"->4,
-      "eta"->0.05,
+//      "eta"->0.05,
       "colsample_bytree"->0.9,
       "subsample"->0.8,
       "verbose_eval"->0
     )
 
     val xgbEstimator = new XGBoostEstimator(xgboostParam)
+//            .setPredictionCol(predictCol)
     val paramGrid = new ParamGridBuilder()
-            .addGrid(xgbEstimator.round, Array(20, 50))
-            .addGrid(xgbEstimator.eta, Array(0.1, 0.4))
+            .addGrid(xgbEstimator.round, Array(20, 50,100,200))
+            .addGrid(xgbEstimator.eta, Array(0.05,0.1, 0.4))
             .build()
     val tv=new TrainValidationSplit()
             .setEstimator(xgbEstimator)
             .setEvaluator(new RegressionEvaluator())
             .setEstimatorParamMaps(paramGrid)
             .setTrainRatio(0.8)
-    val tvModel=tv.fit(train_df)
+
+    val tvModel=tv.fit(train_df.withColumnRenamed(labelCol,"label"))
+      val result_df=tvModel.transform(test_df.withColumnRenamed(labelCol,"label"))
+              .withColumnRenamed("label",labelCol)
+              .withColumnRenamed("prediction",predictCol)
+              .select("user_id",labelCol,predictCol)
+//      val bestmodel = tvModel.bestModel.asInstanceOf[PipelineModel]
+//      val lrModel:Transformer=bestmodel.stages(0)
+//      println("round:"+lrModel.explainParam(xgbEstimator.round))
+//      println("eta:"+lrModel.explainParam(xgbEstimator.eta))
+      tvModel.write.overwrite().save(basePath+"model/tsModel")
+      result_df
   }
 }
