f35dcdad7cc2fa8e1b829773aa11cabf28fec17b
jdlzy <kobeliuziyang@gmail.com>
2018-10-18 23:00:34 +0800

    修改测试部分训练集ID

--- src/main/scala/org/lzy/kaggle/googleAnalytics/GASimpleWithTransmogriAIMain.scala
+++ src/main/scala/org/lzy/kaggle/googleAnalytics/GASimpleWithTransmogriAIMain.scala
@@ -51,7 +51,7 @@ object GASimpleWithTransmogriAIMain extends CustomerFeatures {
     val evaluator = Evaluators.Regression()
       .setLabelCol(totals_transactionRevenue)
       .setPredictionCol(prediction)
-    val trainDataReader = DataReaders.Simple.csvCase[Customer](path = Option(Constants.trainPath))
+    val trainDataReader = DataReaders.Simple.csvCase[Customer](path = Option(Constants.trainPath),key = _.fullVisitorId.toString)
 //    val util=new Utils(spark)
 //    val train_DS=util.readToCSV(Constants.trainPath).as[Customer]
 
