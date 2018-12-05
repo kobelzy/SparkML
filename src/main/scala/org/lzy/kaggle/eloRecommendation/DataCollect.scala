b55eb28171b6e45d54f64ab4588844c5eba662ef
jdlzy <kobeliuziyang@gmail.com>
2018-12-05 23:14:00 +0800

    聚合基础数据，测试通过

--- src/main/scala/org/lzy/kaggle/eloRecommendation/DataCollect.scala
+++ src/main/scala/org/lzy/kaggle/eloRecommendation/DataCollect.scala
@@ -1,5 +1,7 @@
 package org.lzy.kaggle.eloRecommendation
 
+import java.sql.Timestamp
+
 import common.{SparkUtils, Utils}
 import org.lzy.kaggle.eloRecommendation.EloConstants.basePath
 
@@ -15,20 +17,20 @@ object DataCollect {
     * avg_purchases_lag6,active_months_lag6,avg_sales_lag12,avg_purchases_lag12,active_months_lag12,category_4,city_id,state_id,category_2
     */
 
-  case class caseAll(card_id:String,first_active_month:String,feature_1:Int,feature_2:Int,feature_3:Int,
-                     merchant_group_id:String,numerical_1:Int,numerical_2:Int,merchant_category_1:Int,merchant_category_2:Int,merchant_category_4:Int,merchant_city_id:Int,merchant_state_id:Int,merchant_subsector_id:Int,
-                     most_recent_sales_range:Int,most_recent_purchases_range:Int,avg_sales_lag3:Int,avg_purchases_lag3:Int,active_months_lag3:Int,avg_sales_lag6:Int,avg_purchases_lag6:Int,active_months_lag6:Int,avg_sales_lag12:Int,avg_purchases_lag12:Int,active_months_lag12:Int,
-                     authorized_flag:String        ,city_id:String,category_1:Int,installments:Int,category_3:Int,merchant_category_id:Int,merchant_id:String    ,month_lag:Int,purchase_amount:Int,purchase_date :String     ,category_2:Int,state_id:Int,subsector_id:Int
+  case class caseAll(card_id:String, first_active_month:String, feature_1:Int, feature_2:Int, feature_3:Int,
+                     merchant_group_id:String, numerical_1:Double, numerical_2:Double, category_1_merchant:String, category_2_merchant:Double, category_4_merchant:String, city_id_merchant:String, state_id_merchant:String, subsector_id_merchant:String,
+                     most_recent_sales_range:String, most_recent_purchases_range:String, avg_sales_lag3:Double, avg_purchases_lag3:Double, active_months_lag3:Int, avg_sales_lag6:Double, avg_purchases_lag6:Double, active_months_lag6:Int, avg_sales_lag12:Double, avg_purchases_lag12:Double, active_months_lag12:Int,
+                     authorized_flag:String, city_id:String, category_1:String, installments:Int, category_3:String, merchant_category_id:String, merchant_id:String, month_lag:Int, purchase_amount:Int, purchase_date :Timestamp, category_2:Double, state_id:String, subsector_id:String,
+                     target:Double
                     )
   def collect()={
   val spark=SparkUtils.getSpark("collect")
     import spark.implicits._
     val utils=new Utils(spark)
-
     val train_df=utils.readToCSV(EloConstants.trainPath)
     val merchants_df=utils.readToCSV(EloConstants.merchants)
       .select($"merchant_group_id",$"merchant_category_id",$"numerical_1",$"numerical_2",
-        $"category_1".alias("merchant_category_1"),$"category_2".alias("merchant_category_2"),$"category_4".alias("merchant_category_4"),$"city_id".alias("merchant_city_id"),$"state_id".alias("merchant_state_id"),$"subsector_id".alias("merchant_subsector_id"),
+        $"category_1".alias("category_1_merchant"),$"category_2".alias("category_2_merchant"),$"category_4".alias("category_4_merchant"),$"city_id".alias("city_id_merchant"),$"state_id".alias("state_id_merchant"),$"subsector_id".alias("subsector_id_merchant"),
         $"most_recent_sales_range",$"most_recent_purchases_range",$"avg_sales_lag3",$"avg_purchases_lag3",$"active_months_lag3",$"avg_sales_lag6",$"avg_purchases_lag6",$"active_months_lag6",$"avg_sales_lag12",$"avg_purchases_lag12",$"active_months_lag12"
       )
     val newTransactions_df=utils.readToCSV(EloConstants.newMerChantTransactions_mini)
@@ -39,6 +41,7 @@ object DataCollect {
     val all_df=train_df      .join(transactions_df,"card_id")
       .join(merchants_df,"merchant_category_id")
 all_df.show(false)
+    all_df.printSchema()
 
 
   }
