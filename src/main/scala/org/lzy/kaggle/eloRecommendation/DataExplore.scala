4eee1b98576e505af2bf4f8c2178c2fc1ec5a7db
jdlzy <kobeliuziyang@gmail.com>
2018-12-06 17:53:17 +0800

    数据探索

--- src/main/scala/org/lzy/kaggle/eloRecommendation/DataExplore.scala
+++ src/main/scala/org/lzy/kaggle/eloRecommendation/DataExplore.scala
@@ -5,7 +5,9 @@ import org.apache.spark.sql.functions._
 object DataExplore {
   def main(args: Array[String]): Unit = {
 //  run1()
-    merchantsExplor()
+//    merchantsExplor()
+//    explorNewAndHistory
+    exploreCardId
   }
 
 
@@ -58,5 +60,40 @@ object DataExplore {
     val ThanOneMerchants=merchants_df.groupBy("merchant_id").count().filter("count > 1").select("merchant_id").collect().map(_.getString(0))
     merchants_df.filter(row=>ThanOneMerchants.contains(row.getString(0))).sort("merchant_id").show(false)
 
+  }
+
+
+  def explorNewAndHistory()={
+
+          val spark=SparkUtils.getSpark("elo")
+    val utils=new Utils(spark)
+
+
+    val newMerChantTransactions_df=utils.readToCSV(EloConstants.newMerChantTransactions_mini)
+
+
+
+    val historical_df=utils.readToCSV(EloConstants.historical_mini)
+    newMerChantTransactions_df.join(historical_df,"card_id").show(false)
+  }
+
+
+  def exploreCardId()={
+    val spark=SparkUtils.getSpark("elo")
+    val utils=new Utils(spark)
+    val train_df=utils.readToCSV(EloConstants.trainPath)
+
+
+    val test_df=utils.readToCSV(EloConstants.testPath)
+//    val train_group=train_df.groupBy("card_id").count()
+//    val train_count=train_group.filter("count > 1")
+//    println(train_group.count())
+//    println(train_count.count())
+//    train_count.show(false)
+
+
+    val joined=train_df.join(test_df,"card_id")
+    println(joined.count())
+    joined.show(false)
   }
 }
