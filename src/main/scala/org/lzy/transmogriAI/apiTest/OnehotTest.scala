99f1ae61b948eb42917d4a1ef44e43ce1362a5ad
jdlzy <kobeliuziyang@gmail.com>
2019-03-10 23:00:26 +0800

    独热编码测试

--- src/main/scala/org/lzy/transmogriAI/apiTest/OnehotTest.scala
+++ src/main/scala/org/lzy/transmogriAI/apiTest/OnehotTest.scala
@@ -8,6 +8,8 @@ import org.apache.spark.util.SparkUtils
 import org.lzy.kaggle.eloRecommendation.Record
 import com.salesforce.op.features.types._
 import com.salesforce.op.features.{FeatureBuilder, FeatureLike}
+import org.apache
+import org.apache.spark
 
   case class ds(id:Int,category:String)
 class feature{
@@ -15,17 +17,26 @@ class feature{
 
 object OnehotTest {
 
-  implicit val spark=SparkUtil.getSpark()
-  spark.sparkContext.setLogLevel("WARN")
-  import spark.implicits._
+
   def main(args: Array[String]): Unit = {
-  run()
+    val map=scala.collection.immutable.Map(1->2)
+    val m2=scala.collection.immutable.HashMap(1->2,2->3)
+    val hash0=map.##
+    val level=5
+    val a:Int=(hash0 >>> level) & 0x1f
+    println(0x1f)
+println(a)
+
+//  run()
   }
   val id = FeatureBuilder.ID[ds].extract(_.id.toString.toID).asPredictor
   val category = FeatureBuilder.PickList[ds].extract(_.category.toPickList).asPredictor
 
 
   def run()={
+    implicit val spark=SparkUtil.getSpark()
+    spark.sparkContext.setLogLevel("WARN")
+    import spark.implicits._
     val df = spark.createDataset(Seq( (1, "b"), (2, "c"), (3, "a"), (4, "a"), (5, "c"))).toDF("id","category").as[ds]
     df.show(false)
 
@@ -43,6 +54,7 @@ object OnehotTest {
           .setDropLast(false)
     val onehotdata=encoder.transform(indexed)
     onehotdata.show(false)
+
 
   }
 }
