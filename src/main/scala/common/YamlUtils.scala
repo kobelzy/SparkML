f21a068a68f839d36b8f4a8b2e6a728ea700b2bd
jdlzy <kobeliuziyang@gmail.com>
2019-03-20 22:26:26 +0800

    单例模式重构

--- src/main/scala/common/YamlUtils.scala
+++ src/main/scala/common/YamlUtils.scala
@@ -7,22 +7,23 @@ import scala.beans.BeanProperty
 
 
 class YamlUtils extends Serializable {
-  @BeanProperty var minInterval: Int  =0
+  @BeanProperty var minInterval: Int = 0
   @BeanProperty var maxInterval: Int = -1
   @BeanProperty var details: java.util.HashMap[String, Object] = null
 }
-object YamlUtils{
-  def getParam={
-    val yaml=new Yaml
-    val in = this.getClass.getResourceAsStream("/param.yml")
-    println("解析")
-    yaml.loadAs(in,classOf[YamlUtils])
-  }
+
+object YamlUtils {
+  val yaml = new Yaml
+  val in = this.getClass.getResourceAsStream("/param.yml")
+  val yamlUtils = yaml.loadAs(in, classOf[YamlUtils])
+  println("解析")
+
+
 
   def main(args: Array[String]): Unit = {
-    val params=YamlUtils.getParam
+    val params = YamlUtils.yamlUtils
     println(params.minInterval)
-    println(params.details)
-    println(params.details.get("age").asInstanceOf[Int])
+    println(YamlUtils.yamlUtils.details)
+    println(YamlUtils.yamlUtils.details.get("age").asInstanceOf[Int])
   }
 }
